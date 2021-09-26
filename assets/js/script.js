2d094d08c0ba7f43f8226226a16298b227c52ed6
raigormax <raigormax@gmail.com>
2021-09-25 19:54:07 -0500

    Eliminando algunas lineas del html innecesarias, y modificando los estilos para mostrarlo de una mejor manera, además de agregar eventos para controlar los botones

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,7 @@ let auto_play = document.querySelector('#auto');
 let present = document.querySelector('#present');
 let total = document.querySelector('#total');
 let artist = document.querySelector('#artist');
+let rango = document.querySelector('#rango');
 
 let timer;
 let autoplay = 0;
@@ -22,9 +23,17 @@ let playing_song = false;
 //creando el elemento audio
 let radio = new Audio('https://stream.zeno.fm/sg11xrm1qa0uv');
 //funcion para reproducir la radio stream
-play.addEventListener('click', function() {
+play.addEventListener('click', () => {
   radio.play();
 });
 stop.addEventListener('click', () => {
   radio.pause();
-})
+});
+//rango del volumen
+rango.oninput =(() => {
+  let value = rango.value;
+  volume_show.textContent = value;
+  radio.volume = value/parseFloat(100);
+  //console.log(value/parseFloat(100));
+});
+
